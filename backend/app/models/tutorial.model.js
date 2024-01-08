d8032126a3528dbcbf827e01fde9d9d1e4c3cbcc
Stachis-hub <65723617+Stachis-hub@users.noreply.github.com>
2024-01-08 10:28:22 +0100

    Création du projet Backend (Node) et du premier controlleur

---
+++ backend/app/models/tutorial.model.js
@@ -0,0 +1,15 @@
+module.exports = (sequelize, Sequelize) => {
+    const Product = sequelize.define("products", {
+      title: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      published: {
+        type: Sequelize.BOOLEAN
+      }
+    });
+  
+    return Product;
+  };
