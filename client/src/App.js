9108d7620af8004d5e04b9cea98716686e92e3f0
arsene <fopoar@gmail.com>
2024-01-25 07:53:32 -0400

    suppression de fichier apres mise a jour de gitignore

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import Preloader from './components/partials/website/preloader';
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, BrowserRouter } from 'react-router-dom';
@@ -15,55 +14,36 @@ import PharmaFooter from './components/partials/pharma/Footer';
 import PharmaShop from './pages/pharma/shop';
 import PharmaHeader from './components/partials/pharma/header';
 
-=======
-import React, { useState, useEffect } from "react";
-import { Routes, Route, Link } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
 
-import AuthService from "./services/auth-service";
-
-import Login from "./components/Login";
-import Register from "./components/Register";
-import Home from "./components/Home";
-import Profile from "./components/Profile";
-import BoardUser from "./components/BoardUser";
-import BoardModerator from "./components/BoardModerator";
-import BoardAdmin from "./components/BoardAdmin";
-
-const App = () => {
-  const [showModeratorBoard, setShowModeratorBoard] = useState(false);
-  const [showAdminBoard, setShowAdminBoard] = useState(false);
-  const [currentUser, setCurrentUser] = useState(undefined);
-
-  useEffect(() => {
-    const user = AuthService.getCurrentUser();
-
-    if (user) {
-      setCurrentUser(user);
-      setShowModeratorBoard(user.roles.includes("ROLE_MODERATOR"));
-      setShowAdminBoard(user.roles.includes("ROLE_ADMIN"));
-    }
-  }, []);
-
-  const logOut = () => {
-    AuthService.logout();
-  };
->>>>>>> origin/Create_DB
-
+function App() {
   return (
-<<<<<<< HEAD
     <>
-    <header className="header" >
-      <WebsiteTopBar />
-    </header>
-    <div className="header-inner">
-        <div className="container">
-          <div className="inner">
-               <WebsiteMenu />
+  
+    {
+    
+      window.location.href.includes("pharma") ?
+      <PharmaHeader />
+       :    
+      ( <header className="header" > <WebsiteTopBar /> </header>)
+    
+    }
+
+    {
+      window.location.href.includes("pharma") ?
+      <div> j</div>
+      :    
+    ( 
+        <div className="header-inner">
+          <div className="container">
+            <div className="inner">
+                <WebsiteMenu />
+            </div>
           </div>
-        </div>
-    </div>
+      </div>
+    )
+    
+    }
+    
     <BrowserRouter>
     <Routes>
       <Route path="/" element={< HomePage/>} />
@@ -85,6 +65,6 @@ const App = () => {
 	
     </>
   );
-};
+}
 
 export default App;
