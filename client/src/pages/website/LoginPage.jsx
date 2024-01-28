182af02ae498f77b88b4614a53e3f79338eaf2b5
Stachis-hub <65723617+Stachis-hub@users.noreply.github.com>
2024-01-28 19:49:16 +0100

    error on google login

--- client/src/pages/website/LoginPage.jsx
+++ client/src/pages/website/LoginPage.jsx
@@ -3,6 +3,10 @@ import { Form, Input, message } from "antd";
 import { Link, useNavigate } from 'react-router-dom'
 import { useState, useEffect } from 'react'
 import axios from 'axios'
+import googleIcon from "../../assets/images/GoogleIcon.svg";
+import AlternateEmailIcon from "@mui/icons-material/AlternateEmail";
+import HttpsIcon from "@mui/icons-material/Https";
+import { FormControl, TextField, InputAdornment, Button } from "@mui/material";
 
 
 const LoginPage = () => {
@@ -11,6 +15,9 @@ const LoginPage = () => {
     const [password, setPassword] = useState("")
     const [validationErrors, setValidationErrors] = useState({});
     const [isSubmitting, setIsSubmitting] = useState(false);
+    const signInWithGoogle = () => {
+        window.open("http://localhost:3000/auth/google", "_self", 'toolbar=no, scrollbars=yes, resizable=no, width=1000, height=auto')
+    }
 
     const onFinishHandler = (values) => {
         console.log(values)
@@ -54,35 +61,35 @@ const LoginPage = () => {
                                     <div class="row">
                                         <div class="col-lg-6">
                                             <div class="form-group">
-                                            <Form.Item name="email" rules={[{required: true, message: 'Please input your email!'}]}>
-                                            <Input placeholder="Email" required 
-                                            onChange={(e)=>{setEmail(e.target.value)}}
-                                            />
-                                            {validationErrors.email != undefined &&
-                                                <div className="flex flex-col">
-                                                    <small  className="text-danger">
-                                                    {validationErrors.email[0]}
-                                                    </small >
-                                                </div>
-                                            }
-                                        </Form.Item>
+                                                <Form.Item name="email" rules={[{ required: true, message: 'Please input your email!' }]}>
+                                                    <Input placeholder="Email" required
+                                                        onChange={(e) => { setEmail(e.target.value) }}
+                                                    />
+                                                    {validationErrors.email != undefined &&
+                                                        <div className="flex flex-col">
+                                                            <small className="text-danger">
+                                                                {validationErrors.email[0]}
+                                                            </small >
+                                                        </div>
+                                                    }
+                                                </Form.Item>
                                             </div>
                                         </div>
                                         <div class="col-lg-6">
                                             <div class="form-group">
-                                            <Form.Item name="password" rules={[{required: true, message: 'Please input your password!'}]}>
-                                            <Input placeholder="Password" required 
-                                            onChange={(e)=>setPassword(e.target.value)}
-                                            />
-                                            {validationErrors.password != undefined &&
-                                                <div className="flex flex-col">
-                                                    <small  className="text-danger">
-                                                    {validationErrors.password[0]}
-                                                    </small >
-                                                </div>
-                                            }
-                                            
-                                        </Form.Item>
+                                                <Form.Item name="password" rules={[{ required: true, message: 'Please input your password!' }]}>
+                                                    <Input placeholder="Password" required
+                                                        onChange={(e) => setPassword(e.target.value)}
+                                                    />
+                                                    {validationErrors.password != undefined &&
+                                                        <div className="flex flex-col">
+                                                            <small className="text-danger">
+                                                                {validationErrors.password[0]}
+                                                            </small >
+                                                        </div>
+                                                    }
+
+                                                </Form.Item>
                                             </div>
                                         </div>
                                     </div>
@@ -98,6 +105,15 @@ const LoginPage = () => {
                                         </div>
                                     </div>
                                 </Form>
+
+                                <div>
+                                    <Button onClick={signInWithGoogle} variant="outlined">
+                                        <img src={googleIcon}  />
+                                        <p class="gtext" >
+                                            Sign in with Google
+                                        </p>
+                                    </Button>
+                                </div>
 
                             </div>
                         </div>
